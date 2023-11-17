ffefdf52b634cf06754df03299d960ba9b842015
raginwild <raginwild4972@outlook.fr>
2023-11-17 12:32:35 +0100

    init DB et installation sqlalchemy/psycopg2

--- test_postgres.py
+++ test_postgres.py
@@ -1,8 +1,12 @@
 #test injection de données dans la base de données 
 import psycopg2
 from tabulate import tabulate
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+import pandas as pd
 
-#settings connexion DB PostGres (pris depuis pgAdmin 4)
+#settings connexion DB PostGres (paramétrés avec pgAdmin4)
 db_params = {
     'dbname': 'mgl_db',
     'user': 'postgres',
@@ -10,3 +14,4 @@ db_params = {
     'host': 'localhost',
     'port': '5432'
 }
+
